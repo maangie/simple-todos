b2e5edccfc3c4bc98900ed9596de7fabe384d3cb
maangie <maangie@users.noreply.github.com>
2017-03-20 18:31:50 +0900

    9.3 Replace insert with addTask method

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -45,12 +45,7 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', newTask);
 
     // Clear form
     this.newTask = '';
